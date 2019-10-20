e0d490a102b00d2c21ab4d5068c59b442a07119f
Percy Quezada <percy.098@hotmail.com>
2019-10-20 18:14:10 -0500

    Pantalla de Home

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-6">
+                <h2>Mantenimientos:</h2>
+                <div class="d-flex flex-column justify-content-center">
+                    <button class="btn">Clientes</button>
+                    <button class="btn">Préstamos</button>
+                    <button class="btn">Campañas</button>
+                    <button class="btn">Cuentas de Ahorro</button>
+                    <button class="btn">Clientes especiales</button>
+                    <button class="btn">Expedientes de Venta</button> 
+                </div>
+            </div>
+            <div class="col-6">
+                <h2>Reportes:</h2>
+                <div class="d-flex flex-column justify-content-center mb-5">
+                    <button class="btn">Reporte de Cuentas</button>
+                    <button class="btn">Reporte de Préstamos</button>
+                </div>
+                <h2 class="mt-4">Configuración:</h2>
+                <div class="d-flex flex-column justify-content-center">
+                    <button class="btn">Configuración de Parámetros</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style src="@/styles/Home.css" scoped>
+
+</style>
