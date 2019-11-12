c35d085d24e1e235f7eec1ab58218949f408b5e0
Xtoronaldo <32776818+Xtoronaldo@users.noreply.github.com>
2019-11-12 11:32:06 -0500

    Example BarChart

--- frontend/src/views/ReportsAccount.vue
+++ frontend/src/views/ReportsAccount.vue
@@ -1,7 +1,10 @@
 <template>
-<div class="body">
-    <h1> HOLA </h1>
-</div>
+    <div class="container">
+        <div class="Chart">
+            <h1 style="text-align:center;">Barchart</h1>
+            <bar-example/>
+        </div>  
+    </div>
 </template>
 
 
@@ -10,11 +13,72 @@ import {mapState, mapActions} from 'vuex'
 import * as adminDA from '@/dataAccess/adminDA.js'
 import Swal from 'sweetalert2'
 
+/* Ronaldo */
+import BarExample from '@/util/BarExample'
 export default {
-    
+    components: {
+        BarExample
+    },
+    data () {
+      return {
+        dataPoints: null,
+        height: 20
+      }
+    },
+    mounted () {
+      setInterval(() => {
+        this.fillData()
+      }, 2000)
+    },
+    methods: {
+      increaseHeight () {
+        this.height += 10
+      },
+      getRandomInt () {
+        return Math.floor(Math.random() * (50 - 5 + 1)) + 5
+      },
+      fillData () {
+        this.dataPoints = {
+          labels: ['January' + this.getRandomInt(), 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+          datasets: [
+            {
+              label: 'Data One',
+              backgroundColor: '#f87979',
+              data: [this.getRandomInt(), this.getRandomInt(), this.getRandomInt(), this.getRandomInt(), this.getRandomInt(), this.getRandomInt(), this.getRandomInt(), this.getRandomInt(), this.getRandomInt(), this.getRandomInt(), this.getRandomInt(), this.getRandomInt()]
+            }
+          ]
+        }
+      }
+    },
+    computed: {
+      myStyles () {
+        return {
+          height: `${this.height}px`,
+          position: 'relative'
+        }
+      }
+    }
 }
 </script>
 
-<style src="@/styles/CRUDClient.css" scoped>
-
+<style>
+  .container {
+    max-width: 800px;
+    margin: 0 auto;
+  }
+  h1 {
+    font-family: 'Helvetica', Arial;
+    color: #464646;
+    text-transform: uppercase;
+    border-bottom: 1px solid #f1f1f1;
+    padding-bottom: 15px;
+    font-size: 28px;
+    margin-top: 0;
+  }
+  .Chart {
+    padding: 20px;
+    box-shadow: 0px 0px 20px 2px rgba(0, 0, 0, .4);
+    border-radius: 20px;
+    margin: 50px 0;
+  }
 </style>
