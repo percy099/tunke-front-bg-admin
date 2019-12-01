74bff9b56ba0198e3d23063371fd798af594fd83
unknown <kaytlin.espinoza@pucp.edu.pe>
2019-11-30 20:46:45 -0500

    spanish to english

--- frontend/src/views/ClientAccounts.vue
+++ frontend/src/views/ClientAccounts.vue
@@ -111,7 +111,28 @@ export default {
         ...mapState(['token','clientCreate','accountsByClient'])
     },
     mounted(){
-        $('#mydatatable').DataTable();
+        $('#mydatatable').DataTable({
+            "language" : {
+                "sProcessing":     "Procesando...",
+                "sLengthMenu":     "Mostrar _MENU_ registros",
+                "sZeroRecords":    "No se encontraron resultados",
+                "sEmptyTable":     "Ningún dato disponible en esta tabla =(",
+                "sInfo":           "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+                "sInfoEmpty":      "Mostrando registros del 0 al 0 de un total de 0 registros",
+                "sInfoFiltered":   "(filtrado de un total de _MAX_ registros)",
+                "sInfoPostFix":    "",
+                "sSearch":         "Buscar:",
+                "sUrl":            "",
+                "sInfoThousands":  ",",
+                "sLoadingRecords": "Cargando...",
+                "oPaginate": {
+                    "sFirst":    "Primero",
+                    "sLast":     "Último",
+                    "sNext":     "Siguiente",
+                    "sPrevious": "Anterior"
+                }
+            }
+        });
 	},
 }
 </script>
